79a9233687e9708545cff7987ad1952160b0ea69
Luis Miguel Del Gallego Horta <alucardluis@gmail.com>
2017-12-21 00:41:27 -0500

    Markup sidebar

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Image} from "react-native";
+import {Image, StyleSheet, View} from "react-native";
 import {
   Button,
   Text,
@@ -7,17 +7,50 @@ import {
   List,
   ListItem,
   Content,
-  Icon
+  Icon,
+  Thumbnail
 } from "native-base";
 
 class SideBar extends Component {
   render() {
     return (
-      <Container>
-        <Text>Hola</Text>
+      <Container style={styles.container}>
+        <View style={styles.containerHeader}>
+          <Thumbnail source={require('../accets/fotico.jpg')} style={styles.photo} />
+          <Text>
+            Luis Miguel Del Gallego
+          </Text>
+        </View>
+        <Content style={styles.containerList}>
+          <Text>Lista</Text>
+        </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  },
+  containerHeader:{
+    backgroundColor: '#25b36f',
+    height: 144,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 24,
+    paddingVertical: 16
+  },
+  photo:{
+    width: 56,
+    height: 56,
+    marginBottom: 28
+  },
+  containerList:{
+    flex:1,
+    backgroundColor: '#fafafa',
+    paddingHorizontal: 24,
+    paddingVertical: 16
+  }
+});
 
 export default SideBar;
